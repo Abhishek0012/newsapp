bdaa8eca72594a406f3632c168dcc2772cd3bc73
Abhishek <abhishektyagihere@gmail.com>
2022-06-02 12:46:32 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import './App.css'
+import Navbar from './components/Navbar'
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        Hello I am learning react
+        <Navbar/>
       </div>
     )
   }
